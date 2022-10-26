9db31cb718b3debae19f728512abb4aee8a2e070
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2022-10-26 15:51:52 -0700

    WIP push deletedTodos to completedTodos

---
+++ src/components/CompletedTodos.js
@@ -0,0 +1,19 @@
+// import React from "react";
+
+// const CompletedTodos = ({ todos, checkedTodo }) => {
+//   const checkedTodos = todos.map((todo) => (
+//     <div key={Math.random()} onClick={() => checkedTodo}>
+//       {todo.completed && (
+//         <ul>
+//           <li>
+//             {todo.todo}
+//             {todo.date.toString()}
+//           </li>
+//         </ul>
+//       )}
+//     </div>
+//   ));
+//   return <div>{checkedTodos}</div>;
+// };
+
+// export default CompletedTodos;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,13 @@ import Datepicker from "react-datepicker";
 import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 import classes from "./TodoList.module.css";
-
 import Todo from "./Todo";
+import CompletedTodos from "./CompletedTodos";
+
 const TodoList = () => {
   const [userInput, setUserInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [completedTodos, setCompletedTodos] = useState([]);
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
@@ -27,15 +29,24 @@ const TodoList = () => {
       date: moment(date).format(" dddd, MMMM Do YYYY, h:mm a"),
     };
     setTodos([...todos, todoItem]);
+
     localStorage.setItem("todos", JSON.stringify([...todos, todoItem]));
+    localStorage.setItem(
+      "completed",
+      JSON.stringify([...completedTodos, todoItem])
+    );
     setUserInput("");
     console.log(todos);
   };
 
   const deleteTodo = (td) => {
-    const filteredTodos = todos.filter((todo) => todo !== td);
-    setTodos(filteredTodos);
-    localStorage.setItem("todos", JSON.stringify(filteredTodos));
+    const filterIncompletedTodos = todos.filter((todo) => todo !== td);
+    setTodos(filterIncompletedTodos);
+    localStorage.setItem("todos", JSON.stringify(filterIncompletedTodos));
+
+    const filterCompletedTodos = todos.filter((todo) => todo == td);
+    setCompletedTodos(filterCompletedTodos);
+    localStorage.setItem("completed", JSON.stringify(filterCompletedTodos));
   };
   const deleteAllTodo = () => {
     setTodos([]);
@@ -48,36 +59,46 @@ const TodoList = () => {
   };
 
   return (
-    <div className={classes.homepage}>
-      <h2>
-        Get It
-        <b> Together </b>
-      </h2>
-      <label>
-        <input
-          className={classes["input-box"]}
-          type="text"
-          placeholder="Create a new to do item..."
-          onChange={inputHandler}
-          value={userInput}
-        />
-      </label>
-      <h4>
-        Due Date
-        <Datepicker
-          selected={date}
-          onChange={dateHandler}
-          name="date"
-          timeInputLabel="Time:"
-          dateFormat="MMMM d, yyyy h:mm aa"
-          showTimeInput
-        />
-        <button type="submit" onClick={addTodo} disabled={userInput.length < 1}>
-          Add
-        </button>
-        <button onClick={deleteAllTodo}> Clear</button>
-      </h4>
-      <Todo todos={todos} deleteTodo={deleteTodo} />
+    <div>
+      <div className={classes.homepage}>
+        <h2>
+          Get It
+          <b> Together </b>
+        </h2>
+        <label>
+          <input
+            className={classes["input-box"]}
+            type="text"
+            placeholder="Create a new to do item..."
+            onChange={inputHandler}
+            value={userInput}
+          />
+        </label>
+        <h4>
+          Due Date
+          <Datepicker
+            selected={date}
+            onChange={dateHandler}
+            name="date"
+            timeInputLabel="Time:"
+            dateFormat="MMMM d, yyyy h:mm aa"
+            showTimeInput
+          />
+          <button
+            type="submit"
+            onClick={addTodo}
+            disabled={userInput.length < 1}
+          >
+            Add
+          </button>
+          <button onClick={deleteAllTodo}> Clear</button>
+        </h4>
+        <Todo todos={todos} deleteTodo={deleteTodo} />
+      </div>
+      <div>
+        <h2>Completed Todos</h2>
+        {/* <CompletedTodos todos={todos} completedTodos={completedTodos} /> */}
+      </div>
     </div>
   );
 };
